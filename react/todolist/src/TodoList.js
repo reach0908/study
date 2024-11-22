b33256851a77a9fdebeb6f6d082ca254ab8bddef
reach0908 <wlsghkim0908@gmail.com>
2024-11-22 22:07:19 +0900

    feat(todo): 기본적인 투두 리스트 앱 만들기

---
+++ react/todolist/src/TodoList.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+import TodoItems from "./TodoItems";
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+    };
+    this.addItem = this.addItem.bind(this);
+  }
+  addItem(e) {
+    var itemArray = this.state.items;
+
+    if (this._inputElement.value !== "") {
+      itemArray.unshift({
+        text: this._inputElement.value,
+        key: Date.now(),
+      });
+
+      this.setState({
+        items: itemArray,
+      });
+
+      this._inputElement.value = "";
+    }
+
+    console.log(itemArray);
+
+    e.preventDefault();
+  }
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input
+              placeholder="enter task"
+              ref={(a) => (this._inputElement = a)}
+            />
+            <button type="submit">add task</button>
+          </form>
+        </div>
+        <TodoItems entries={this.state.items} />
+      </div>
+    );
+  }
+}
+
+export default TodoList;
