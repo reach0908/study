3236fb120d235e22a72e21413abda5d3974b7e4d
reach0908 <wlsghkim0908@gmail.com>
2024-11-23 11:20:03 +0900

    feat(task): 태스크 제거 및 애니메이션 추가

--- react/todolist/src/TodoList.js
+++ react/todolist/src/TodoList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import TodoItems from "./TodoItems";
+import "./TodoList.css";
 
 class TodoList extends Component {
   constructor(props) {
@@ -9,6 +10,7 @@ class TodoList extends Component {
       items: [],
     };
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
   addItem(e) {
     var itemArray = this.state.items;
@@ -30,6 +32,17 @@ class TodoList extends Component {
 
     e.preventDefault();
   }
+
+  deleteItem(key) {
+    var filteredItems = this.state.items.filter(function (item) {
+      return item.key !== key;
+    });
+
+    this.setState({
+      items: filteredItems,
+    });
+  }
+
   render() {
     return (
       <div className="todoListMain">
@@ -42,7 +55,7 @@ class TodoList extends Component {
             <button type="submit">add task</button>
           </form>
         </div>
-        <TodoItems entries={this.state.items} />
+        <TodoItems entries={this.state.items} delete={this.deleteItem} />
       </div>
     );
   }
