257355be358f3d3eba3aff48200b86cacb71d454
reach0908 <wlsghkim0908@gmail.com>
2024-11-25 06:51:20 +0900

    refactor(menuButton): Purecomponent로 불필요한 렌더링 방지

--- react/slidingmenu/src/MenuButton.js
+++ react/slidingmenu/src/MenuButton.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import "./MenuButton.css";
 
-class MenuButton extends React.Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return false;
-  }
+class MenuButton extends PureComponent {
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   if (nextProps.handleMouseDown === this.props.handleMouseDown) {
+  //     return false;
+  //   }
+  //   return true;
+  // }
   render() {
     console.log("Rendering MenuButton");
     return (
